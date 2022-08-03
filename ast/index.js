d9acef1354a60c3f8fdbbc87d9a6eef698f18e7d
刘启明 <455043818@qq.com>
2022-08-03 18:18:56 +0800

    feat: docs update

---
+++ ast/index.js
@@ -0,0 +1,19 @@
+const recast = require('recast')
+
+// 测试函数字符串
+const code =
+  `
+  function add(a, b) {
+    return a +
+      // 有什么奇怪的东西混进来了
+      b
+  }
+  `
+
+const ast = recast.parse(code)
+
+const fun = recast.print(ast).code
+
+console.log(fun, 'fun')
+
+// recast.run(code,)
