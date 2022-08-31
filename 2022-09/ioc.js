bfbad79cf7a9cb8bcf0df39938031706cb0afd87
刘启明 <455043818@qq.com>
2022-08-31 20:32:12 +0800

    feat： ioc初步解耦

--- 2022-09/ioc.js
+++ 2022-09/ioc.js
@@ -1,13 +1,19 @@
 // https://zhuanlan.zhihu.com/p/125024256
 
 class Rate {
+  init(order) {
+    order.rate = this;
+  }
   star(count) {
     console.log(`您对订单的评价为${count}星`)
   }
 }
 
 
-class Share{
+class Share {
+  init(order) {
+    order.share = this;
+  }
   shareTo(platform){
       switch (platform) {
           case 'wx':
@@ -24,16 +30,32 @@ class Share{
 }
 
 class Order {
+  // 用于维护依赖关系的Map
+  static modules = new Map();
+
   constructor() {
-    this.rate = new Rate()
-    this.share = new Share()
+    // this.rate = new Rate()
+    // this.share = new Share()
+    for (let module of Order.modules.values()) {
+      // 调用模块init方法
+      module.init(this);
+    }
+  }
+
+  static inject(module) {
+    Order.modules.set(module.constructor.name, module);
   }
 
   getInfo() {
     console.log('这里是订单的信息')
   }
 }
+const rate = new Rate();
+// 注入依赖
+Order.inject(rate);
 
+const share = new Share();
+Order.inject(share);
 let order = new Order()
 order.getInfo()
 order.rate.star(5)
