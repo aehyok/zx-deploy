04efd46d89dc263e3823cac6ccf532016fcf0357
刘启明 <455043818@qq.com>
2023-09-29 00:32:36 +0800

    chore:  9月29日

--- src/sql/export-sql.sh
+++ src/sql/export-sql.sh
@@ -10,6 +10,6 @@ mysqldump -h192.168.0.104 -P4006  -uroot -psunlight2021 metadata tj_zdyzbdyb>/us
 ## 代码表一级表数据 
 mysqldump -h192.168.0.104 -P4006  -uroot -psunlight2021 dvsdb30 dm_reftabledata>/usr/local/sunlight/sql/dm_reftabledata.sql; 
 
-## 导出所有的Collect数据定义
+## 导出所有的Collect以及pcs2309开头的表结构 不包含数据
 # mysqldump -h192.168.0.104 -P4006  -uroot -psunlight2021 -d dvsdb30 $(mysql -h192.168.0.104 -P4006  -uroot -psunlight2021 -D dvsdb30 -Bse "show tables like 'Collect%'") > /usr/local/sunlight/sql/Collect.sql
 # mysqldump -h192.168.0.104 -P4006  -uroot -psunlight2021 -d dvsdb30 $(mysql -h192.168.0.104 -P4006  -uroot -psunlight2021 -D dvsdb30 -Bse "show tables like 'pcs2309_%'") > /usr/local/sunlight/sql/pcs2309_.sql
